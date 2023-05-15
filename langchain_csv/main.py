63a913dc0813bdf28aeafceb5863b2f94490b401
Bruce-CodeMan <1033684650@qq.com>
2023-05-15 16:45:02 +0800

    update the langchain_csv

---
+++ langchain_csv/main.py
@@ -0,0 +1,49 @@
+'''
+Date: 2023-05-12 14:59:21
+Author: Bruce
+Description: 
+'''
+from json import load
+import os
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+# Set the Environment variable, just change your own account
+os.environ["OPENAI_API_KEY"] = "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"
+
+loader = CSVLoader(file_path="./price.csv",
+                    csv_args={
+                        'delimiter': ',',
+                        'fieldnames': [
+                            'prodName', 
+                            'prodCat', 
+                            'lowPrice', 
+                            'highPrice', 
+                            'avgPrice', 
+                            'pubDate', 
+                            'unitInfo', 
+                            'place'
+                        ]
+                    })
+
+data = loader.load()
+
+print(data[0])
+
+# Download embeddings from OpenAI
+embeddings = OpenAIEmbeddings()
+
+# Store the vectors
+db = Chroma.from_documents(data, embeddings)
+
+# Use RetrievalQA Chain
+retriever = db.as_retriever()
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever, input_key="question")
+
+while True:
+    query = input("Please input the question what you want to know?\n")
+    resp = qa({"question": query})
+    print(resp)
