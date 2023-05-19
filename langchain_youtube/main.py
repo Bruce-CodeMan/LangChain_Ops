417218f5d9b461b4e27c1f4a7c7bd9f76f3d0c52
Bruce Hsu <1033684650@qq.com>
2023-05-19 17:30:48 +0800

    Using pytube to change the mp3 into the srt

--- langchain_youtube/main.py
+++ langchain_youtube/main.py
@@ -1,4 +1,5 @@
 import os
+import whisper
 from langchain.llms import OpenAI
 from langchain.document_loaders import YoutubeLoader
 from pytube import YouTube
@@ -11,16 +12,27 @@ os.environ["OPENAI_API_KEY"] = "sk-IzUSTzA9a0koyJ4jL8R0T3BlbkFJjTQ5f2fFfKQoDkI2a
 
 # print(result)
 
-url = "https://www.youtube.com/watch?v=C_78DM8fG6E"
+# url = "https://www.youtube.com/watch?v=C_78DM8fG6E"
 
-video = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+# video = YouTube(url, use_oauth=True, allow_oauth_cache=True)
 
-audio = video.streams.filter(only_audio=True).first()
+# audio = video.streams.filter(only_audio=True).first()
 
-video_title = video.streams[0].title
+# video_title = video.streams[0].title
 
-video.streams.get_highest_resolution().filesize
+# video.streams.get_highest_resolution().filesize
 
-audio = video.streams.get_audio_only()
+# audio = video.streams.get_audio_only()
 
-audio.download(output_path="temp")
+# audio.download(output_path="temp")
+
+model = whisper.load_model("base")
+
+mp3_file_path = "./temp/sample.mp4"
+
+# About one minute
+transcription = model.transcribe(mp3_file_path)
+
+res = transcription["text"]
+
+print(res)
