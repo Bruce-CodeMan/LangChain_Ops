3cacfaea1e00950be5e00ccbfa0dd259f561673b
Bruce Hsu <1033684650@qq.com>
2023-05-23 10:28:02 +0800

    finish the langchain_pdf GUI

--- langchain_pdf_gui/app.py
+++ langchain_pdf_gui/app.py
@@ -3,15 +3,16 @@
 @Author        : Bruce Hsu
 @Description   : 
 '''
-import os
-from io import BytesIO
-import base64
-from pathlib import Path
-import tempfile
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.document_loaders import PyPDFLoader
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
 
 def main():
     load_dotenv()
@@ -21,9 +22,35 @@ def main():
     # Upload the pdf File
     pdf = st.file_uploader("Upload your PDF", type="pdf")
 
+    # Extract the pdf file
     if pdf is not None:
-        loader = PyPDFLoader(pdf)
-        print(loader)
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+
+        # Split into the chunks
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        chunks = text_splitter.split_text(text)
+
+        # st.write(chunks)
+
+        # Create the embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        user_question = st.text_input("Ask a question about your pdf:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+            
+            st.write(response)
 
 if __name__ == '__main__':
     main()
